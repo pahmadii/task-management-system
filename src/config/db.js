227ecf1514b5b7e943e4e908325e09dec62e27d3
Parisa <fairyahmadi71@gmail.com>
2025-08-22 21:48:14 +0330

    updated db connection added(require dotenv & db-name-password...and err message)

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,27 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('task_manager', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT || 3306,
+    logging: false, 
+  }
+);
+
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected successfully...');
+  } catch (err) {
+    console.error(' Unable to connect to the database:', err);
+  }
+})();
 
 module.exports = sequelize;
 
