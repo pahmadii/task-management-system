51b79e83a31316d0965722a8b5cffe293b4595b7
Parisa <fairyahmadi71@gmail.com>
2025-08-22 22:12:40 +0330

    Implement auth routes with validation and authorization

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const { register, login } = require('../controllers/authController');
+const { registerValidation, loginValidation } = require('../middleware/validators.js');
+const authorizeRoles = require('../middleware/authorizeRoles');
 const router = express.Router();
-const authController = require('../controllers/authController');
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/register', registerValidation, register);
+router.post('/login', loginValidation, login);
+
+router.get('/admin-dashboard', authorizeRoles('admin'), (req, res) => {
+  res.json({ message: 'Welcome Admin!' });
+});
+
+router.get('/user-dashboard', authorizeRoles('user', 'admin'), (req, res) => {
+  res.json({ message: 'Welcome User or Admin!' });
+});
 
 module.exports = router;
