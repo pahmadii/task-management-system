069b926ee2e5b89bb5b8e1dbf818ecc2ddf3d698
Parisa <fairyahmadi71@gmail.com>
2025-08-22 19:31:32 +0330

    complete setup project structure and implement auth register/login

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const authRoutes = require('./routes/authRoutes');
+const errorMiddleware = require('./middleware/errorMiddleware');
 
 
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
+app.use(errorMiddleware);
 
 
 const PORT = process.env.PORT || 5000;
