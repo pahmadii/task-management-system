17b4ff43c6b0ad1dc2cb6ec62eb4558e0d8184e3
Parisa <fairyahmadi71@gmail.com>
2025-08-24 14:34:29 +0330

    remove users routers before slash/

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,15 +7,15 @@ const userController = require('../controllers/userController');
 router.use(authorizeRoles('admin'));
 
 // دریافت لیست همه کاربران
-router.get('/users', userController.getAllUsers);
+router.get('/', userController.getAllUsers);
 
 // بروزرسانی کامل اطلاعات کاربر
-router.put('/users/:id', userController.updateUser);
+router.put('/:id', userController.updateUser);
 
 // تغییر نقش کاربر
-router.patch('/users/:id/role', userController.changeUserRole);
+router.patch('/:id/role', userController.changeUserRole);
 
 // حذف کاربر و تمام تسک‌های او
-router.delete('/users/:id', userController.deleteUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
