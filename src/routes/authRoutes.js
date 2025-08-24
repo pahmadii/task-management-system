1a490c24dd6c05be332fc02035f7555d15ad875e
Parisa <fairyahmadi71@gmail.com>
2025-08-24 14:42:47 +0330

    remove admin dashbord to auth route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,12 +7,5 @@ const router = express.Router();
 router.post('/register', registerValidation, register);
 router.post('/login', loginValidation, login);
 
-router.get('/admin-dashboard', authorizeRoles('admin'), (req, res) => {
-  res.json({ message: 'Welcome Admin!' });
-});
-
-router.get('/user-dashboard', authorizeRoles('user', 'admin'), (req, res) => {
-  res.json({ message: 'Welcome User or Admin!' });
-});
 
 module.exports = router;
