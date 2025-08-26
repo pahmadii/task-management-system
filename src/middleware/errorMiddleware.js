c8b86764bb0b794b3a936e21bca7fad3c803d003
Parisa <fairyahmadi71@gmail.com>
2025-08-26 10:05:17 +0330

    update errmiddleware and add token json multer err

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,10 +1,27 @@
+const logger = require('../utils/logger');  
+
+
 module.exports = (err, req, res, next) => {
-    console.error(err.stack);
-    if (err.name === 'SequelizeUniqueConstraintError') {
+ logger.error(err.stack);
+
+  if (res.headersSent) return next(err);
+
+  if (err.name === 'SequelizeUniqueConstraintError') {
     return res.status(400).json({ message: 'Duplicate field value', error: err.message });
   }
   if (err.name === 'SequelizeValidationError') {
     return res.status(400).json({ message: 'Validation error', error: err.errors.map(e => e.message) });
   }
-    res.status(500).json({ message: 'Something went wrong', error: err.message });
+  if (err.name === 'TokenExpiredError') {
+    return res.status(401).json({ message: 'Token expired' });
+  }
+  if (err.name === 'JsonWebTokenError') {
+    return res.status(401).json({ message: 'Invalid or expired token', error: err.message });
+  }
+  if (err.name === 'MulterError') {
+    return res.status(400).json({ message: 'File upload error', error: err.message });
+  }
+
+return res.status(400).json({ message: 'An unexpected error occurred. Please try again.', error: err.message });
+
 };
