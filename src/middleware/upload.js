8a85295e953cb62a6c383a4dc971b1ba4d386e2f
Parisa <fairyahmadi71@gmail.com>
2025-08-26 08:56:23 +0330

    feat(updoads) create upload middleware and used multer file-type

---
+++ src/middleware/upload.js
@@ -0,0 +1,56 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const { fileTypeFromFile } = require("file-type");
+const { FILE_TYPES, MAX_FILE_SIZE } = require("../utils/constants");
+
+if (!fs.existsSync("uploads")) fs.mkdirSync("uploads");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const fileFilter = async (req, file, cb) => {
+  try {
+    // ذخیره موقت فایل برای بررسی نوع
+
+    const tempPath = path.join(
+      __dirname,
+      "..",
+      "uploads",
+      `temp-${Date.now()}${path.extname(file.originalname)}`
+    );
+    await new Promise((resolve, reject) => {
+      const ws = fs.createWriteStream(tempPath);
+      file.stream.pipe(ws).on("finish", resolve).on("error", reject);
+    });
+
+    // بررسی نوع فایل با file-type
+
+    const fileType = await fileTypeFromFile(tempPath);
+
+    fs.unlinkSync(tempPath); // حذف فایل موقت
+
+    if (fileType && FILE_TYPES.includes(fileType.mime)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only JPEG, PNG, and PDF files are allowed"), false);
+    }
+  } catch (err) {
+    cb(new Error("Error validating file type"), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: MAX_FILE_SIZE },
+});
+
+module.exports = upload;
