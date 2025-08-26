28ec18c0ea08fee6bf763ff8ab6606686beb205d
Parisa <fairyahmadi71@gmail.com>
2025-08-26 10:01:45 +0330

    In app.js Update All routes - add exprss syn err

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
+require('express-async-errors')
 const express = require('express');
 const app = express();
 require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const profileRoutes = require('./routes/profileRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 const errorMiddleware = require('./middleware/errorMiddleware');
 
 
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/users',userRoutes)
+app.use('/api/users',userRoutes);
+app.use('/api/profile', profileRoutes);
+app.use('/api/tasks', taskRoutes);
 
 app.use(errorMiddleware);
 
