740f537a385dce901fb5ef37df84fd86821a0719
Parisa <fairyahmadi71@gmail.com>
2025-08-26 09:00:21 +0330

    chore(constants): add roles and file upload limits

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ROLES: {
+    USER: "user",
+    ADMIN: "admin",
+  },
+  FILE_TYPES: ["image/jpeg", "image/png", "application/pdf"],
+  MAX_FILE_SIZE: 5 * 1024 * 1024, // 5MB
+};
